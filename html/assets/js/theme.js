8f2a283b4e5983b7ca90ab9e03b11eea64bc2718
Daniel Moran Vilchez <113327439+DanielMoranV@users.noreply.github.com>
2024-05-18 07:34:42 -0500

    Deploy init

--- html/assets/js/theme.js
+++ html/assets/js/theme.js
@@ -1,59 +1,88 @@
-
-$('document').ready(function() {
-  $('#doctorSlideshow').owlCarousel({
+$("document").ready(function () {
+  $("#doctorSlideshow").owlCarousel({
     nav: true,
     dots: false,
-    navText: ["<span class='mai-arrow-back'></span>", "<span class='mai-arrow-forward'></span>"],
+    navText: [
+      "<span class='mai-arrow-back'></span>",
+      "<span class='mai-arrow-forward'></span>",
+    ],
     responsive: {
       0: {
-        items: 1
+        items: 1,
       },
       576: {
-        items: 2
+        items: 2,
       },
       992: {
-        items: 3
-      }
-    }
+        items: 3,
+      },
+    },
+  });
+  $("#clientSlideshow").owlCarousel({
+    nav: true,
+    dots: false,
+    navText: [
+      "<span class='mai-arrow-back'></span>",
+      "<span class='mai-arrow-forward'></span>",
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 2,
+      },
+      768: {
+        items: 3,
+      },
+      1200: {
+        // Ajuste para resoluciones mayores donde se mostrarán 5 ítems
+        items: 5,
+      },
+    },
   });
 });
 
-$('document').ready(function() {
-  $("a[data-role='smoothscroll']").click(function(e) {
+$("document").ready(function () {
+  $("a[data-role='smoothscroll']").click(function (e) {
     e.preventDefault();
-    
+
     var position = $($(this).attr("href")).offset().top - nav_height;
 
-    $("body, html").animate({
-        scrollTop: position
-    }, 1000 );
+    $("body, html").animate(
+      {
+        scrollTop: position,
+      },
+      1000
+    );
     return false;
   });
 });
 
-$('document').ready(function() {
+$("document").ready(function () {
   // Back to top
   var backTop = $(".back-to-top");
 
-  $(window).scroll(function() {
-    if($(document).scrollTop() > 400) {
-      backTop.css('visibility', 'visible');
-    }
-    else if($(document).scrollTop() < 400) {
-      backTop.css('visibility', 'hidden');
+  $(window).scroll(function () {
+    if ($(document).scrollTop() > 400) {
+      backTop.css("visibility", "visible");
+    } else if ($(document).scrollTop() < 400) {
+      backTop.css("visibility", "hidden");
     }
   });
 
-  backTop.click(function() {
-    $('html').animate({
-      scrollTop: 0
-    }, 1000);
+  backTop.click(function () {
+    $("html").animate(
+      {
+        scrollTop: 0,
+      },
+      1000
+    );
     return false;
   });
 });
 
-
-$('document').ready(function() {
+$("document").ready(function () {
   // Tooltips
   $('[data-toggle="tooltip"]').tooltip();
 
@@ -63,4 +92,3 @@ $('document').ready(function() {
   // Page scroll animate
   new WOW().init();
 });
-
